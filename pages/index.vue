9f881d10e856ff201055ebb656871c8ae2e54774
Paul Gallo <paul.gallo@octo.com>
2022-01-26 15:21:34 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Bienvenue sur le blog de Ruse</h1>
+    <p>Site en construction :)</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
