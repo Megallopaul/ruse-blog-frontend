013d0d8f57469ebf06c910e7765460acd9157329
Paul Gallo <paul.gallo@octo.com>
2022-03-02 16:54:42 +0100

    fix potential 404 error page (because of axios interceptor)

--- repositories/index.js
+++ repositories/index.js
@@ -1,20 +1,9 @@
 import axios from 'axios'
 import strapiRepository from './strapiRepository'
 
-const strapiLoginData = {
-  identifier: process.env.STRAPI_AUTH_ID,
-  password: process.env.STRAPI_AUTH_PASSWORD
-}
-
-const strapiClient = axios.create({ baseURL: process.env.STRAPI_URL })
-
-strapiClient.interceptors.request.use(async req => {
-  if (req.headers['Authorization'] === undefined) {
-    const { data } = await axios.post(`${process.env.STRAPI_URL}/auth/local`, strapiLoginData)
-    req.headers['Authorization'] = `Bearer ${data.jwt}`
-  }
-
-  return req
+const strapiClient = axios.create({
+  baseURL: process.env.STRAPI_URL,
+  headers: { 'Authorization': `Bearer ${process.env.STRAPI_TOKEN}` }
 })
 
 const blogRepository = new strapiRepository(strapiClient)
